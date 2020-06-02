5ce9529ed10d0c1dad36614fe1909fee0eff776d
Josef <jxb@mit.edu>
2020-06-02 11:22:21 -0400

    wip

--- vol-control.py
+++ vol-control.py
@@ -461,7 +461,7 @@ class OSVController(Thread):
         #self.motor.SetEncM1(self.ROBOCLAW_ADDRESS, -self.MAX_ENCODER_COUNT)
         while not self.hallEffectEvent.is_set():
             status = self.motor.ReadError(self.ROBOCLAW_ADDRESS)
-            logging.info(f'Motor status {status:04X}', end='\r')
+            logging.info(f'Motor status {status}', end='\r')
             self.motor.SpeedAccelDeccelPositionM1(self.ROBOCLAW_ADDRESS, 500, 250,500, 0, 0)
         self.motor.ResetEncoders(self.ROBOCLAW_ADDRESS)
         self.hallEffectEvent.clear()  
