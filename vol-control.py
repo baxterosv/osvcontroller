68433735654eca34760cfd9dd8d03ac2c33e84cd
Josef <jxb@mit.edu>
2020-06-02 12:21:07 -0400

    wip

--- vol-control.py
+++ vol-control.py
@@ -457,7 +457,7 @@ class OSVController(Thread):
         #self.motor.SetEncM1(self.ROBOCLAW_ADDRESS, -self.MAX_ENCODER_COUNT)
         while not self.hallEffectEvent.is_set():
             status = self.motor.ReadError(self.ROBOCLAW_ADDRESS)
-            logging.info(f'Motor status {status}\r')
+            #logging.info(f'Motor status {status}\r')
             #self.motor.SpeedAccelDeccelPositionM1(self.ROBOCLAW_ADDRESS, 500, 250,500, 0, 0)
         logging.info('Hall Effect Triggered')
         self.motor.ResetEncoders(self.ROBOCLAW_ADDRESS)
